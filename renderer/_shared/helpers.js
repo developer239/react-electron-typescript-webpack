bea00dbb9f055ae6b242b7a50c9ffe3b4d7a5201
michaljarnot <m.jarnot@yahoo.com>
2018-01-15 19:57:45 +0100

    Move create window function into _shared helpers folder

--- renderer/_shared/helpers.js
+++ renderer/_shared/helpers.js
@@ -1,28 +1,8 @@
 import electron from 'electron';
+import { createWindow as createNewWindow } from '../../_shared/windowHelper';
 
 
 const { remote: { BrowserWindow } } = electron;
 
 
-const property = (obj, key) => obj && obj.hasOwnProperty(key) ? obj[key] : false;
-
-export const createWindow = (createdWindow) => (name, options) => {
-  createdWindow = new BrowserWindow({
-    show: false,
-    width: property(options, 'width') || 800,
-    height: property(options, 'height') || 800,
-    toolbar: property(options, 'toolbar') || false,
-    resizable: property(options, 'resizable') || true,
-  });
-
-  createdWindow.setResizable(true);
-  createdWindow.loadURL('file://' + __dirname + `/../../renderer/${name}/index.html`);
-
-  createdWindow.once('ready-to-show', () => {
-    createdWindow.show();
-  });
-
-  createdWindow.on('closed', () => {
-    createdWindow = null;
-  });
-};
+export const createWindow = createNewWindow(BrowserWindow);
--- window/_shared/helpers.js
+++ window/_shared/helpers.js
@@ -1,25 +1,8 @@
-import { BrowserWindow } from 'electron';
+import electron from 'electron';
+import { createWindow as createNewWindow } from '../../_shared/windowHelper';
 
 
-const property = (obj, key) => obj && obj.hasOwnProperty(key) ? obj[key] : false;
+const { BrowserWindow } = electron;
 
-export const createWindow = (createdWindow) => (name, options) => {
-  createdWindow = new BrowserWindow({
-    show: false,
-    width: property(options, 'width') || 800,
-    height: property(options, 'height') || 800,
-    toolbar: property(options, 'toolbar') || false,
-    resizable: property(options, 'resizable') || true,
-  });
 
-  createdWindow.setResizable(true);
-  createdWindow.loadURL('file://' + __dirname + `/../../renderer/${name}/index.html`);
-
-  createdWindow.once('ready-to-show', () => {
-    createdWindow.show();
-  });
-
-  createdWindow.on('closed', () => {
-    createdWindow = null;
-  });
-};
+export const createWindow = createNewWindow(BrowserWindow);
